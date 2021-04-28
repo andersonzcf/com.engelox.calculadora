46599284755755b40b55518f68ee21df9d787ad9
Wrubleski <henrike.hip@live.com>
2021-04-28 12:04:36 -0300

    fix exit option on menu (#12)

--- src/com/engelox/calculadora/Main.java
+++ src/com/engelox/calculadora/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
       do {
         option = menu.getOperation();
-        if (option >= 0) {
+        if (option > 0) {
           app.execute(option);
         }
       } while (option != 0);
