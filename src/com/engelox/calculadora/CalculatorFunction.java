7f944d9120fa3c87c5b6afd71c7bfa396a7d09b9
Anderson <andersonfcz@bol.com.br>
2021-04-30 20:50:27 -0300

    remove boolean field for separation of concerns (#4)

--- src/com/engelox/calculadora/CalculatorFunction.java
+++ src/com/engelox/calculadora/CalculatorFunction.java
@@ -1,20 +1,5 @@
 package com.engelox.calculadora;
 
 public enum CalculatorFunction {
-<<<<<<< HEAD
-	SUM(true), SUBTRACTION(true), DIVISION(true), MULTIPLCATION(true), SIN(false), COS(false), CIRCLE(false),
-	SPHERE(false);
-
-	private boolean multipleInput;
-
-	CalculatorFunction(boolean multipleInput) {
-		this.multipleInput = multipleInput;
-	}
-
-	public boolean isMultipleInput() {
-		return this.multipleInput;
-	}
-=======
 	SUM, SUBTRACTION, DIVISION, MULTIPLCATION, SIN, COS, CIRCLE, SPHERE;
->>>>>>> develop
 }
